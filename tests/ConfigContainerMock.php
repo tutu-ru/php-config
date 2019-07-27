42beaade8d1af233c304a6616bf969514030eadd
Sereda Ilya <sereda@tutu.ru>
2019-07-28 02:30:19 +0300

    Mock

---
+++ tests/ConfigContainerMock.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace TutuRu\Tests\Config;
+
+class ConfigContainerMock extends \TutuRu\Config\ConfigContainer
+{
+    private $runtimeStorage = [];
+
+    public function getValue(string $path, bool $required = false, $defaultValue = null)
+    {
+        if (array_key_exists($path, $this->runtimeStorage)) {
+            return $this->runtimeStorage[$path];
+        }
+
+        return parent::getValue($path, $required, $defaultValue);
+    }
+
+    public function setValue(string $path, $value = null): void
+    {
+        if (is_null($value)) {
+            unset($this->runtimeStorage[$path]);
+        } else {
+            $this->runtimeStorage[$path] = $value;
+        }
+    }
+}
